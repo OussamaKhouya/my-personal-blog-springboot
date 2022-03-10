74935cf2d92bbb8de50bcebd41ebc8f200e00512
OussamaKhouya <oukhouya0b@gmail.com>
2022-03-10 19:03:03 +0100

    Basic-FullStack-App:Send Data to angular

--- src/main/java/ml/oussamakhouya/mypersonalblog/dao/BlogPostRepository.java
+++ src/main/java/ml/oussamakhouya/mypersonalblog/dao/BlogPostRepository.java
@@ -2,6 +2,8 @@ package ml.oussamakhouya.mypersonalblog.dao;
 
 import ml.oussamakhouya.mypersonalblog.entity.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 }
